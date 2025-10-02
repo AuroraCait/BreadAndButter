88db9d154d00743d1c99511f162116e438e3a153
unknown <mas9lol@gmail.com>
2025-10-01 22:49:34 -0500

    Added Interactable Zone scripting, to be added to zone objects in editor

---
+++ Assets/Scripts/Gameplay/PlayerInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Core;
+using Platformer.Model;
+using UnityEngine;
+
+namespace Platformer.Gameplay
+{
+    /// <summary>
+    /// Fired when the player can interact with a zone.
+    /// </summary>
+    /// <typeparam name="PlayerInteractable"></typeparam>
+    public class PlayerInteractable : Simulation.Event<PlayerInteractable>
+    {
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            var player = model.player;
+            Debug.LogWarning("Player can interact in this zone");
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/InteractionZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// InteractionZone components mark a collider which will schedule a
+    /// PlayerEnteredInteractionZone event when the player enters the trigger.
+    /// </summary>
+    public class InteractionZone : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                var ev = Schedule<PlayerEnteredInteractionZone>();
+                ev.interactionzone = this;
+            }
+        }
+    }
+}
