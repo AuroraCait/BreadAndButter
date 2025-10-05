f46df0bc277ec384b76a2eaa83f032f0086b8763
Spencer Warneke <sorbet.dev@proton.me>
2025-10-05 00:07:17 -0700

    Door SFX

--- Assets/Scripts/WalkInKCI.cs
+++ Assets/Scripts/WalkInKCI.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class WalkInKCI : MonoBehaviour
 {
     private Animator m_Animator;
+    private AudioSource m_AudioSource;
+    private bool m_closed;
+
+    public AudioClip m_DoorOpenClip;
+    public AudioClip m_DoorCloseClip;
 
     public void Start()
     {
         m_Animator = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
+        m_closed = true;
     }
 
     public void OnComboAdvanced()
     {
         m_Animator.SetTrigger("Toggle");
+
+        m_AudioSource.PlayOneShot(m_closed ? m_DoorOpenClip : m_DoorCloseClip);
+
+        m_closed = !m_closed;
     }
 }
